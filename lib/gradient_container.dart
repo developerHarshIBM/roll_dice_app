680d120b8f8260922e47fab32ecc385b237f9da3
Harsh Anand <developersharsh25@gmail.com>
2023-07-27 23:03:59 +0530

    project finalized on 27 july 2023

---
+++ lib/gradient_container.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/dice_roller.dart';
+
+final startAlignment = Alignment.topLeft;
+final endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple()
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
